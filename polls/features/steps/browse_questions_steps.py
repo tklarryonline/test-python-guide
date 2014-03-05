32f8e90225c196b409fc0315d4561d819c4cd577
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-03-05 17:26:00 +0700

    Fixed spacing in codebase

--- polls/features/steps/browse_questions_steps.py
+++ polls/features/steps/browse_questions_steps.py
@@ -26,7 +26,6 @@ def i_have_a_list_of_polls(step):
 def i_visit_url(step, url):
     browser().get('http://localhost:8000%s' % url) # produce right url
 
-
 @step(u'I should see a list of polls question')
 def i_should_see_a_list_of_polls_question(step):
     questions = browser().find_elements_by_css_selector("#questions .question")
